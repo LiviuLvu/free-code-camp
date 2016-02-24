e17fb0816c086da769cf1ef272db5e7086e96915
Liviu Iancu <lvudesign@gmail.com>
2016-02-24 18:00:25 +0200

    finished jQuerry section on camp
    just basic selectors stuff here animated with animate.css library

--- jqery/js/main.js
+++ jqery/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-   $('button').addClass('animated bounce');
+   $('#target1').css('color', 'red');
+   $('#target1').prop('disabled', true);
+   $('#target2').appendTo('#right-well');
+   $('#target5').clone().appendTo('#left-well');
+   $('#target1').parent().css('background-color', 'red');
+   $("#right-well").children().css("color", "orange");
+   $(".target:nth-child(2)").addClass("animated bounce");
+   $(".target:even").addClass("animated shake");
+   $("body").addClass("animated hinge");
 });
