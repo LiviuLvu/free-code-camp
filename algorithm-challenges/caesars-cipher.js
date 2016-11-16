5851f3845c41ddbfb6e670c56b84862c043d060c
Liviu <liviuiancu@gmx.com>
2016-11-16 23:02:09 +0200

    working on cipher

--- algorithm-challenges/caesars-cipher.js
+++ algorithm-challenges/caesars-cipher.js
@@ -2,22 +2,25 @@
 
 function rot13(str) {
 // LBH QVQ VG!
-
+  var letters = /^[A-Za-z]+$/;
   // loop through string
   for (var i = 0; i < str.length; i++) {
+    // only affect letters and ignore other non alphanumeric characters
+    if (str[i].match(letters)) {
+      console.log('found a letter');
+    }
 
     // offset character by 13 characters
-    character = str.charCodeAt(0) - 13;
-
+    character = str.charCodeAt(i) - 13;
     var newChar = String.fromCharCode(character);
 
-    console.log(newChar);
+    // console.log(newChar);
 
   }
 
   return str;
 }
 
-rot13('N');
+rot13('TE ST');
 
 // rot13("SERR PBQR PNZC");
