4b08abe10caa7a85a5507d03e4d7debc0a245626
Liviu <liviuiancu@gmx.com>
2016-11-17 22:04:26 +0200

    decoded FCC cipher challenge

--- algorithm-challenges/caesars-cipher.js
+++ algorithm-challenges/caesars-cipher.js
@@ -1,18 +1,31 @@
-// keep non alphabetical characters unchanged
-var decodedStr = '';
 function rot13(str) {
-// LBH QVQ VG!
-  var letters = /[A-Z]/;
-  // loop through string
+  // save the decoded string in a new variable
+  var decodedStr = '';
+
   for (var i = 0; i < str.length; i++) {
+
     // only affect letters and ignore other non alphanumeric characters
+    var letters = /[A-Z]/;
+
     if (str[i].match(letters)) {
+      // get numerical code for character;
       // offset character by 13 characters
-      characterCode = str.charCodeAt(i) - 1;
+      characterCode = str.charCodeAt(i)-13;
+
+      if (characterCode < 65) {
+        // if characterCode is smaller than A, offset alphabet length;
+        characterCode += 26;
+      }
+
+      // convert code to string
       var newChar = String.fromCharCode(characterCode);
 
       decodedStr += newChar;
       console.log(decodedStr);
+    }
+    else {
+      // add the unchanged characters to the decoded string
+      decodedStr += str[i];
     }
 
   }
@@ -20,6 +33,6 @@ function rot13(str) {
   return decodedStr;
 }
 
-rot13('A B C D');
+// rot13('A, M! N. Z');
 
-// rot13("SERR PBQR PNZC");
+rot13('LBH QVQ VG!');
