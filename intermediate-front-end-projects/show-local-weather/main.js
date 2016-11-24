200d9cad685f6e0822128079efda3574a72cb9b6
Liviu <liviuiancu@gmx.com>
2016-11-25 00:29:26 +0200

    loading data from wunderground, completed F/C units switch

--- intermediate-front-end-projects/show-local-weather/main.js
+++ intermediate-front-end-projects/show-local-weather/main.js
@@ -1,92 +1,89 @@
+// DESCRIPTION OF FUNCTIONALITY
 // get weather in my current location on page load
 // city, temperature
 // icon weather indicator
 // push button to toggle fahrenheit/celsius
 
-// api key 9ae9a5ef6d1fead22bbd77b4b0a615b5
 
 // DATA NOTES
-
+// api key af24923e752f84ec, 9ae9a5ef6d1fead22bbd77b4b0a615b5
 // get location weather based on coordiantes
-// http://api.openweathermap.org/data/2.5/weather?lat=45.7704186&lon=21.224092499999998&appid=9ae9a5ef6d1fead22bbd77b4b0a615b5
-
-
-// // //
-// below code returns very accurate location
-// source: http://stackoverflow.com/questions/9053262/geolocation-html5-enablehighaccuracy-true-false-or-best-option
+// 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=9ae9a5ef6d1fead22bbd77b4b0a615b5'
+// http://api.wunderground.com/api/af24923e752f84ec/geolookup/q/' + latitude + ',' + longitude + '.json
 
 jQuery(document).ready(function($) {
 
-navigator.geolocation.getCurrentPosition(
+
+  // get geolocation
+  navigator.geolocation.getCurrentPosition(
     successCallback,
-    errorCallback_highAccuracy,
-    {maximumAge:600000, timeout:5000, enableHighAccuracy: true}
+    errorCallback_highAccuracy, { maximumAge: 600000, timeout: 5000, enableHighAccuracy: true }
 
-);
+  );
 
-function errorCallback_highAccuracy(error) {
-    if (error.code == error.TIMEOUT)
-    {
-        // Attempt to get GPS loc timed out after 5 seconds,
-        // try low accuracy location
-        $('body').append("attempting to get low accuracy location");
-        navigator.geolocation.getCurrentPosition(
-               successCallback,
-               errorCallback_lowAccuracy,
-               {maximumAge:600000, timeout:10000, enableHighAccuracy: false});
-        return;
+  function errorCallback_highAccuracy(error) {
+    if (error.code == error.TIMEOUT) {
+      // Attempt to get GPS loc timed out after 5 seconds,
+      // try low accuracy location
+      $('.coordinates').text("attempting to get low accuracy location");
+      navigator.geolocation.getCurrentPosition(
+        successCallback,
+        errorCallback_lowAccuracy, { maximumAge: 600000, timeout: 10000, enableHighAccuracy: false });
+      return;
     }
 
-    var msg = "<p>Can't get your location (high accuracy attempt). Error = ";
+    var msg = "Can't get your location (high accuracy attempt). Error = ";
     if (error.code == 1)
-        msg += "PERMISSION_DENIED";
+      msg += "PERMISSION_DENIED";
     else if (error.code == 2)
-        msg += "POSITION_UNAVAILABLE";
-    msg += ", msg = "+error.message;
+      msg += "POSITION_UNAVAILABLE";
+    msg += ", msg = " + error.message;
 
-    $('body').append(msg);
-}
+    $('.coordinates').text(msg);
+  }
 
-function errorCallback_lowAccuracy(error) {
-    var msg = "<p>Can't get your location (low accuracy attempt). Error = ";
+  function errorCallback_lowAccuracy(error) {
+    var msg = "Can't get your location (low accuracy attempt). Error = ";
     if (error.code == 1)
-        msg += "PERMISSION_DENIED";
+      msg += "PERMISSION_DENIED";
     else if (error.code == 2)
-        msg += "POSITION_UNAVAILABLE";
+      msg += "POSITION_UNAVAILABLE";
     else if (error.code == 3)
-        msg += "TIMEOUT";
-    msg += ", msg = "+error.message;
+      msg += "TIMEOUT";
+    msg += ", msg = " + error.message;
 
-    $('body').append(msg);
-}
+    $('.coordinates').text(msg);
+  }
 
-function successCallback(position) {
+  function successCallback(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-    $('body').append("<p>Your location is: " + latitude + "," + longitude+" </p><p>Accuracy="+position.coords.accuracy+"m");
-}
+    $('.coordinates').text("Latitude: " + latitude + ", Longitude: " + longitude + " Accuracy=" + position.coords.accuracy + "m");
+
+    // start working with the data
+    $.getJSON('http://api.wunderground.com/api/af24923e752f84ec/geolookup/conditions/q/' + latitude + ',' + longitude + '.json' + latitude + '&lon=' + longitude + '&appid=9ae9a5ef6d1fead22bbd77b4b0a615b5', function(data) {
+
+      var location = data.location.city;
+      var temperature = data.current_observation.temp_c;
+      $('.temperature').text(temperature + 'C');
+
+      $('.temperatureSwitch').on('click', function(event) {
+        if (temperature === data.current_observation.temp_c) {
+          temperature = data.current_observation.temp_f;
+          $('.temperature').text(temperature + 'F');
+        } else {
+          temperature = data.current_observation.temp_c;
+          $('.temperature').text(temperature + 'C');
+        }
+      });
 
 
-///////////
-// this code is inacurate
-  // if (navigator.geolocation) {
-  //   navigator.geolocation.getCurrentPosition(showPosition);
-  // } else {
-  //   console.log("Geolocation is not supported by this browser.");
-  // }
 
-  // function showPosition(position) {
-  //   console.log("Latitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude);
-  // }
+      // console.log("Current temperature in " + location + " is: " + temp_c);
 
+      // console.log(data);
+    });
 
-
-
-
-  // $.getJSON( 'http://api.openweathermap.org/data/2.5/weather?q=Timisoara,RO&appid=9ae9a5ef6d1fead22bbd77b4b0a615b5', function( data ) {
-  //     // work with the data
-
-  //     console.log(data);
-  //   });
+  }
 
 });
