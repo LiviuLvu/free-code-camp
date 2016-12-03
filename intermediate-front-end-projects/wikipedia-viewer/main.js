1f5d2588db792fe0d6e81182cc31d3dc16cdeddb
Liviu <liviuiancu@gmx.com>
2016-12-03 10:49:49 +0200

    added data from wiki search in console

--- intermediate-front-end-projects/wikipedia-viewer/main.js
+++ intermediate-front-end-projects/wikipedia-viewer/main.js
@@ -3,15 +3,31 @@
 // scrol search results in page
 // click a button to see a random Wikipedia entry.
 
-// https://en.wikipedia.org/wiki/Special:Random
+
+// show error for no results found (if arr.length = 0) {...};
+
+// https://en.wikipedia.org/w/api.php?action=opensearch&limit=20&search=[]&format=json
+// https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=[...]&limit=20&profile=fuzzy&formatversion=2
 
 
 $(document).ready(function() {
-  loadWiki();
+  randomArticle();
+  searchWiki();
 });
 
-function loadWiki () {
+function randomArticle() {
   $('#random').on('click', function(event) {
     window.open('https://en.wikipedia.org/wiki/Special:Random');
+  });
+}
+
+function searchWiki() {
+  $('#searchTerm').on('click', function(e) {
+    var searchVal = $('#searchVal').val();
+    var wikiLink = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + searchVal + '&limit=20&callback=?';
+    $.getJSON(wikiLink, function(data) {
+      console.log(data);
+      $('.results').html('<li>' +  + '</li>');
+    });
   });
 }
