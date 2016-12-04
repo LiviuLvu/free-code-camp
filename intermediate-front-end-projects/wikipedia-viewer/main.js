33cebcf85778f4bc175b729d176476ed0d36a357
Liviu <liviuiancu@gmx.com>
2016-12-04 14:35:09 +0200

    completed functionality: search list based on input

--- intermediate-front-end-projects/wikipedia-viewer/main.js
+++ intermediate-front-end-projects/wikipedia-viewer/main.js
@@ -1,33 +1,82 @@
 // DESCRIPTION OF FUNCTIONALITY
 // search field wiki articles
-// scrol search results in page
+// scrol search results in page, with link to page
 // click a button to see a random Wikipedia entry.
 
-
-// show error for no results found (if arr.length = 0) {...};
-
-// https://en.wikipedia.org/w/api.php?action=opensearch&limit=20&search=[]&format=json
-// https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=[...]&limit=20&profile=fuzzy&formatversion=2
-
+var searchVal = '';
 
 $(document).ready(function() {
   randomArticle();
-  searchWiki();
+
+  $('#searchTerm').on('click', function() {
+    alert();
+    searchWiki();
+  });
+
+  $('input').keyup(function(event) {
+    event.preventDefault();
+    $('.searchLabel').removeClass('alert-box -warning');
+
+    searchVal = $('#searchVal').val();
+    if (searchVal !== '') {
+      searchWiki(); 
+      // clear list if search item is deleted
+    } else {
+      $('.results').html('');
+    }
+
+  });
 });
 
+function alert () {
+  // highligt input if user forgot to write search items 
+  if (searchVal === '') {
+    $('.searchLabel').addClass('alert-box -warning');
+  } else {
+    $('.searchLabel').removeClass('alert-box -warning');
+  }
+}
+
+// open a random article
 function randomArticle() {
-  $('#random').on('click', function(event) {
+  $('#random').on('click', function() {
     window.open('https://en.wikipedia.org/wiki/Special:Random');
   });
 }
 
-function searchWiki() {
-  $('#searchTerm').on('click', function(e) {
-    var searchVal = $('#searchVal').val();
-    var wikiLink = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + searchVal + '&limit=20&callback=?';
-    $.getJSON(wikiLink, function(data) {
-      console.log(data);
-      $('.results').html('<li>' +  + '</li>');
-    });
+// search articles based on user input
+function searchWiki(argument) {
+
+  var wikiLink = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + searchVal + '&limit=20&callback=?';
+
+  if (searchVal !== '' && $('.results').is(':empty')) {
+    // show spinner until data is loaded
+    $('.wait').addClass('spinner');
+  } else {
+    $('.wait').removeClass('spinner');
+    // clear list if search item is deleted
+    $('.results').html('');
+  }
+
+  
+  // load data
+  $.getJSON(wikiLink, function(data) {
+
+    var listResults = '';
+
+
+    for (i = 0; i < data[1].length; i++) {
+      var title = data[1][i];
+      var description = data[2][i];
+      var link = data[3][i];
+
+      listResults += '<a href="' + link + '"><li>';
+      listResults += '<h4>' + title + '</h4>';
+      listResults += '<p>' + description + '</p>';
+      listResults += '</li></a>';
+    }
+
+    $('.wait').removeClass('spinner');
+    $('.results').html(listResults);
   });
 }
