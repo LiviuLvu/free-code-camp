d48d243bafec0227c2bb59bba4855bdf6ed8c29e
Liviu <liviuiancu@gmx.com>
2016-12-09 22:40:57 +0200

    accesing data from twitch with client-id header, jquery ajax

--- intermediate-front-end-projects/twitchtv/main.js
+++ intermediate-front-end-projects/twitchtv/main.js
@@ -4,12 +4,32 @@
 //  I will see a placeholder notification if a streamer has closed their Twitch account (or the account never existed). You can verify this works by adding brunofin and comster404 to your array of Twitch streamers.
 
 
+// Client ID 9epvfqca5rr13kra8rwkdz2jodq57j
+
 $(document).ready(function($) {
   loadChanels();
 });
 
-function loadChanels () {
-  $.getJSON('/path/to/file', {param1: 'value1'}, function(json, textStatus) {
-      /*optional stuff to do after success */
+function loadChanels() {
+  // $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/hebo', function(data) {
+  //     console.log(data);
+  // });
+
+//   $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+//   console.log(data);
+// });
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.twitch.tv/kraken/streams?channel=ESL_SC2,OgamingSC2,cretetion,freecodecamp,storbeck,habathcx,RobotCaleb,noobs2ninjas',
+    headers: {
+     'Client-ID': '9epvfqca5rr13kra8rwkdz2jodq57j'
+    },
+    success: function(data) {
+      console.log(data);
+    }
   });
+
+
+
 }
