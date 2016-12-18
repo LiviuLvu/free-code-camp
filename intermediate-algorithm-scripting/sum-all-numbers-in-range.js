4dcb720d57a38ba8a5450a3df71576ef5da12bd8
Liviu Iancu <liviuiancu@gmx.com>
2016-12-18 09:09:03 +0200

    completed sum-all-numbers-in-range

--- intermediate-algorithm-scripting/sum-all-numbers-in-range.js
+++ intermediate-algorithm-scripting/sum-all-numbers-in-range.js
@@ -1,5 +1,13 @@
 function sumAll(arr) {
-  // return 1;
+  var min = Math.min.apply(null, arr);
+  var max = Math.max.apply(null, arr);
+  var total = 0;
+
+  for (i = min; i <= max; i++) {
+    total += i;
+  }
+
+  return total;
 }
 
 sumAll([1, 4]);
