84d4f64b4d76f13f8d98a11d563875e29d2b8eed
Liviu <liviuiancu@gmx.com>
2016-12-23 19:11:44 +0200

    completed search-and-replace

--- intermediate-algorithm-scripting/search-and-replace.js
+++ intermediate-algorithm-scripting/search-and-replace.js
@@ -6,16 +6,10 @@ function myReplace(str, before, after) {
     after = after.replace(/\b\w/g, function(l) {
       return l.toUpperCase();
     });
-    console.log('uppercase ' + after);
-  } 
-  else if (before[0] !== stringCheck.toUpperCase()) {
-    console.log('lowercase ' + after);
   }
 
-  // does not work
   str = str.replace(before, after);
   return str;
 }
 
-// myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
 myReplace("His name is Tom", "Tom", "john");
