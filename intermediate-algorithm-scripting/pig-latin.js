dc56801f15922491343429a30f556db04222ff9f
Liviu <liviuiancu@gmx.com>
2016-12-23 19:17:33 +0200

    started work on pig-latin algorithm

--- intermediate-algorithm-scripting/pig-latin.js
+++ intermediate-algorithm-scripting/pig-latin.js
@@ -1,9 +1,14 @@
-
 // Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
 
 // If a word begins with a vowel you just add "way" to the end.
-if ('test') {
-  //
+
+function translatePigLatin(str) {
+   
+  if ('test') {
+    //
+  }
+
+  return str;
 }
 
-// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+translatePigLatin("consonant");
