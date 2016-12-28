d7d5532c812a2bc54033fffdfd3a47277260577c
Liviu Iancu <liviuiancu@gmx.com>
2016-12-28 21:34:06 +0200

    basic solution to sum-all-primes algorithm

--- intermediate-algorithm-scripting/sum-all-primes.js
+++ intermediate-algorithm-scripting/sum-all-primes.js
@@ -1,16 +1,22 @@
 function sumPrimes(num) {
-  // check if num is prime
-  for (var i = 2; i <= num; i++) {
-    if (num === i) {
-      console.log('prime number');
-      return num;
-    }
-    else if (num % i === 0) {
-      console.log('not prime');
-      return num;
+  totalPrimes = 0;
+  for (var i = 0; i <= num; i++) {
+    isPrime(i);
+  }
+
+  function isPrime (nrCheck) {
+    // check if num is prime
+    for (var i = 2; i <= nrCheck; i++) {
+      if (nrCheck === i) {
+        console.log(nrCheck + ' nrCheck is prime');
+        totalPrimes += nrCheck;
+      } else if (nrCheck % i === 0) {
+        return 0;
+      }
     }
   }
 
+  return totalPrimes;
 }
 
 sumPrimes(11);
