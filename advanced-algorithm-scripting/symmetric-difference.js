8d71f37283cf645bdacb2cc2f1634fa44ba3f6f3
Liviu Iancu <liviuiancu@gmx.com>
2017-01-04 00:40:49 +0200

    second version solution for symmetric-difference allgorithm

--- advanced-algorithm-scripting/symmetric-difference.js
+++ advanced-algorithm-scripting/symmetric-difference.js
@@ -1,32 +1,32 @@
-function sym(args) {
-  // convert arguments to array
-  args = Array.prototype.slice.call(arguments);
-
-  var getDiff = function(arr1, arr2) {
-
-    function filterFunction(arr1, arr2) {
-      // filter() method creates a new array with all elements that pass the test implemented by the provided function.
-      return arr1.filter(function(element) {
-        // indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
-        // if element in arr1 is not found in arr2, return the element
-        return arr2.indexOf(element) === -1;
-      });
+function sym() {
+    var args = [];
+    for (var i = 0; i < arguments.length; i++) {
+        // for any number of arguments, push each to "args" array
+        args.push(arguments[i]);
     }
 
-    return filterFunction(arr1, arr2)
-      // concat() method is used to merge two or more arrays. This method returns a new array.
-      .concat(filterFunction(arr2, arr1))
-      // create new array
-      .filter(function(element, index, arr) {
-        // "arr" is the current array
-        // "index" if the element in array
-        // indexOf returns the first element found in "arr" if it's same as index
-        return arr.indexOf(element) === index;
-    });
-  };
+    function symDiff(arrayOne, arrayTwo) {
+        var result = [];
 
-  // Reduce all arguments getting the difference of them
-  return args.reduce(getDiff, []);
+        arrayOne.forEach(function(item) {
+            if (arrayTwo.indexOf(item) < 0 && result.indexOf(item) < 0) {
+                // if item does not exist in "arrayTwo" and also not in "result"
+                result.push(item);
+            }
+        });
+
+        arrayTwo.forEach(function(item) {
+            if (arrayOne.indexOf(item) < 0 && result.indexOf(item) < 0) {
+                result.push(item);
+            }
+        });
+
+        return result;
+    }
+
+    // Apply reduce method to args array, using the symDiff function
+    return args.reduce(symDiff);
 }
 
-sym([1, 2, 3], [5, 2, 1, 4], [5, 2, 7]);
+// sym([1, 2, 3], [5, 2, 1, 4], [5, 2, 7]);
+sym([1, 2, 3]);
