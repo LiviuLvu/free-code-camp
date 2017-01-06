78d8e3113a229fdaa1aa20a137dc1f4e0b85c924
Liviu Iancu <liviuiancu@gmx.com>
2017-01-07 01:07:42 +0200

    final code for exact-change algorithm

--- advanced-algorithm-scripting/exact-change2.js
+++ advanced-algorithm-scripting/exact-change2.js
@@ -1,26 +1,26 @@
 // solution from stack overflow
-// need to study and understand code
-function checkCashRegister(price, cash, inDrawer) {
+// renamed some variables and added comments to explain all code
+function checkCashRegister(price, cash, drawer) {
+  // using only cents to calculate change
   var change = 100 * (cash - price);
-  var availableFunds = 0;
-
   var moneyUnits = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000];
+  // record of amount taken out of drawer
   var amtToReturn = [];
 
-  for (var i = inDrawer.length - 1; i >= 0; i--){
-    var amt = 0;
-    while (moneyUnits[i] <= change && inDrawer[i][1] > 0 && change > 0){
-      console.log("subtracting " + moneyUnits[i]);
-      // reduce amount in inDrawer
-      inDrawer[i][1] -= moneyUnits[i]/100;
+  for (var i = drawer.length - 1; i >= 0; i--){
+    var amtFromDrawer = 0;
+    // find largest unit denominator && is there enough in drawer && was all change given to customer?
+    while (moneyUnits[i] <= change && drawer[i][1] > 0 && change > 0){
+      // reduce amount in drawer
+      drawer[i][1] -= moneyUnits[i]/100;
       // reduce amount from change
       change -= moneyUnits[i];
-      // keep track of how much money was taken out of inDrawer
-      amt += moneyUnits[i]/100; 
+      // money taken out of drawer
+      amtFromDrawer += moneyUnits[i]/100; 
     }
-    if (amt !== 0){
-      // adds record of amount taken out of inDrawer
-       amtToReturn.push([inDrawer[i][0], amt]);
+    if (amtFromDrawer !== 0){
+      // [money unit name, amount]
+      amtToReturn.push([drawer[i][0], amtFromDrawer]);
     }
   }
 
@@ -29,9 +29,9 @@ function checkCashRegister(price, cash, inDrawer) {
     return "Insufficient Funds";
   }
 
-  // if there is any money left in inDrawer, it returns amtToReturn
-  for (var j = 0; j < inDrawer.length; j++){
-    if (inDrawer[j][1] > 0){
+  // if there is any money left in drawer
+  for (var j = 0; j < drawer.length; j++){
+    if (drawer[j][1] > 0){
       return amtToReturn;
     }
   }
@@ -40,4 +40,4 @@ function checkCashRegister(price, cash, inDrawer) {
   return "Closed";
 }
 
-checkCashRegister(0.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+checkCashRegister(18.50, 20.00, [["PENNY", 0.00], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 0.00]]);
