2b074f7549da5b937823c5115c1fbf4ea1deafb7
Liviu Iancu <liviuiancu@gmx.com>
2017-01-07 18:25:27 +0200

    cleaned up console logs

--- advanced-algorithm-scripting/inventory-update.js
+++ advanced-algorithm-scripting/inventory-update.js
@@ -1,13 +1,10 @@
 function updateInventory(arr1, arr2) {
+
   arr2.forEach(function addToArr1(element, index) {
-    console.log('forEach arr2' + element);
     for (var i = 0; i < arr1.length; i++) {
-      console.log('for i in arr1');
       // add qty from arr2 to arr1 if same item found
       if (arr1[i][1] === element[1]) {
-        console.log('if found add qty ' + arr1[i][0] + arr1[i][1]);
         arr1[i][0] += element[0];
-        console.log('if found add qty ' + arr1[i][0] + arr1[i][1]);
       }
     }
     // add qty from arr2 to arr1 if item not found in arr1
@@ -15,16 +12,14 @@ function updateInventory(arr1, arr2) {
       return arr1Element[1] !== element[1];
     });
     if (notFound) {
-      console.log('not found in arr1' + element);
       arr1.push(element);
     }
   });
-  
+
   // sort arr1
   return arr1.sort(function (a, b) {
     a = a[1].toUpperCase();
     b = b[1].toUpperCase();
-
     if (a < b) {
       return -1;
     }
