a13f66909f9f2b007a7948a5c6b647bc407ce070
Liviu Iancu <liviuiancu@gmx.com>
2017-01-10 19:02:20 +0200

    update to make algorithm friendly-date-ranges more readable

--- advanced-algorithm-scripting/friendly-date-ranges.js
+++ advanced-algorithm-scripting/friendly-date-ranges.js
@@ -19,37 +19,37 @@ function makeFriendlyDates(arr) {
     var month = "January,February,March,April,May,June,July,August,September,October,November,December"
       .split(",")[unformatedDate.getMonth()];
     var year = unformatedDate.getFullYear();
-    var first = arr[0].split('-');
-    var second = arr[1].split('-');
+    var start = arr[0].split('-');
+    var end = arr[1].split('-');
 
 
     // if the date range ends in less than a year from when it begins, do not display the ending year
-    if (raw === arr[1] && first[0] < second[0] && first[1] >= second[1] && first[2] > second[2] ||
-      raw === arr[1] && first[0] >= second[0] && first[1] > second[1]) {
+    if (raw === arr[1] && start[0] < end[0] && start[1] >= end[1] && start[2] > end[2] ||
+      raw === arr[1] && start[0] >= end[0] && start[1] > end[1]) {
       return month + " " + day + nth(day);
     }
     // if the date range is within the same year, the year should not be displayed at the end
-    if (raw === arr[1] && first[0] === second[0] && first[1] < second[1]) {
+    if (raw === arr[1] && start[0] === end[0] && start[1] < end[1]) {
       return month + " " + day + nth(day);
     }
     // Additionally, if the date range begins in the current year (i.e. it is currently the year 2016)
     // and ends within one year, the year should not be displayed at the beginning of the friendly range.      
-    if (currentYear == first[0] && first[1] >= second[1] && first[2] > second[2] ||
-      currentYear == first[0] && first[1] > second[1]) {
+    if (currentYear == start[0] && start[1] >= end[1] && start[2] > end[2] ||
+      currentYear == start[0] && start[1] > end[1]) {
       return month + " " + day + nth(day);
     }
     // if the range ends in the same month that it begins, do not display the ending year or month nor the beginning year.
     // start of range rules
-    if (raw === arr[0] && arr[0] !== arr[1] && first[0] === second[0] && first[1] === second[1]) {
+    if (raw === arr[0] && arr[0] !== arr[1] && start[0] === end[0] && start[1] === end[1]) {
       return month + " " + day + nth(day);
     } 
     // end of range rules
-    else if (raw === arr[1] && arr[0] !== arr[1] && first[0] === second[0] && first[1] === second[1]) {
+    else if (raw === arr[1] && arr[0] !== arr[1] && start[0] === end[0] && start[1] === end[1]) {
       return day + nth(day);
     }
 
     // special test case for free code camp current year issue
-    if (first[0] == second[0] - 1 && first[1] > second[1]) {
+    if (start[0] == end[0] - 1 && start[1] > end[1]) {
       return month + " " + day + nth(day);
     }
 
@@ -58,7 +58,7 @@ function makeFriendlyDates(arr) {
   }
 
   dateArray.push(cleanDate(arr[0]));
-  // if date range is one day, dont add second date in array
+  // if date range is one day, dont add end date in array
   if (arr[0] !== arr[1]) {
     dateArray.push(cleanDate(arr[1]));
   }
