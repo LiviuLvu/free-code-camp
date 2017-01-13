49e882c6d06cf1f296f6eb88b4cd4c39af7556a1
Liviu Iancu <liviuiancu@gmx.com>
2017-01-13 19:13:28 +0200

    pairwise, testing push and slice to obtain final numbers

--- advanced-algorithm-scripting/pairwise.js
+++ advanced-algorithm-scripting/pairwise.js
@@ -1,22 +1,31 @@
 // find all 2 numbers that return the sum equal to second argument
 // return sum of their indexes
 function pairwise(arr, arg) {
+  tempArray = [];
+
   var sumIndex = arr.reduce(function(acc, add, index, self) {
     var correctNr;
     
-    correctNr = self.find(function (elem) {
-        console.log(add + elem + ' where is 2');
-        return add + elem === arg;
+    correctNr = self.find(function (elem, index2) {
+        if (add + elem === arg) {
+          console.log(arr + 'hey array');
+          arr = arr.slice(index, 1);
+          arr = arr.slice(index2, 1);
+          tempArray.push(index);
+          return self.indexOf(index2);
+        }
     });
     if (correctNr === undefined) {
       correctNr = 0;
     }
-    console.log(correctNr);
-    
+    console.log(tempArray);
     return acc + correctNr;
   }, 0);
 
   return sumIndex;
 }
 
-pairwise([1, 4, 2, 3, 0, 5], 7);
+// pairwise([16, 4, 2, 5, 3, 0], 7);
+// must return 11.
+pairwise([1, 1, 1], 2);
+// should return 1
