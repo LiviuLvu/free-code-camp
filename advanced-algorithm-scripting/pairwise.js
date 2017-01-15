24f6c8840abb253442cbc5f8fa8b9799fd912dc0
Liviu Iancu <liviuiancu@gmx.com>
2017-01-15 03:28:08 +0200

    solved pairwise algorithm

--- advanced-algorithm-scripting/pairwise.js
+++ advanced-algorithm-scripting/pairwise.js
@@ -1,13 +1,9 @@
-// find all 2 numbers that return the sum equal to second argument
-// return sum of their indexes
 function pairwise(arr, arg) {
-  var result = arr.reduce(function(sum, value1, index1, self) {
-    // in here the first index is arr[0];
-    // in the next line slice() returns a new array with the first element removed
-    arr.slice(index1 + 1).forEach(function(value2, index2) {
-      // in here the first index is skipped
-      if (value1 + value2 === arg) {
-        sum += arr.indexOf(value1) + arr.indexOf(value2);
+  var result = arr.reduce(function(sum, currentValue, currentIndex, initialArr) {
+    arr.slice(currentIndex + 1).some(function(value2, i) {
+      if (currentValue + value2 === arg && currentValue !== undefined) {
+        sum += currentIndex + (i + 1 + currentIndex);
+        currentValue = initialArr[i + 1 + currentIndex] = undefined;
       }
     });
     return sum;
@@ -15,9 +11,4 @@ function pairwise(arr, arg) {
 
   return result;
 }
-
-
-// pairwise([1, 4, 2, 3, 0, 5], 7);
-// must return 11
-pairwise([1, 1, 1], 2);
-// should return 1
+pairwise([0, 0, 0, 0, 1, 1], 1);
