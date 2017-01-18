7e80e3dc80f27ee227cfd05e945b4fa4c740ed29
Liviu Iancu <liviuiancu@gmx.com>
2017-01-19 01:55:46 +0200

    added start and reset functionality, with related messages

--- advanced-frontend-projects/pomodoro-timer/main.js
+++ advanced-frontend-projects/pomodoro-timer/main.js
@@ -1,43 +1,62 @@
-// possible solution
 
-function startTimer(duration, display) {
-  var timer = duration, minutes, seconds;
+function TimerModule(timer) {
+  var isOn = false;
+  var interval;
 
-  var cocos = setInterval(function() {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-
+  function count() {
+    console.log(timer);
+    var minutes = parseInt(timer / 60, 10);
+    var seconds = parseInt(timer % 60, 10);
+    // put a 0 in front of one digit
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    display.text(minutes + ":" + seconds);
-
+    $('.time').text(minutes + ":" + seconds);
     --timer;
-
     if (timer < 0) {
-      $('.message').text('Hooray! Break the walls!');
-      clearInterval(cocos);
-    }
-  }, 1000);
+      $('.message').text('Hooray, Timeout!');
+      clearInterval(interval);
+      // to do: call the takeBreak function
+    } 
+  }
 
+  function takeBreak() {
+    // to do: code for the break time
+    // to do: at end of run call count functinon
+  }
+
+  this.start = function() {
+    if (!this.isOn) {
+      interval = setInterval(count.bind(this), 1000);
+      $('.message').text('Go for it!');
+    }
+    this.isOn = true;
+  };
+
+  this.reset = function() {
+    clearInterval(interval);
+    timer = 0;
+    $('.work-val').val(0);
+    $('.message').text('Timer was reset to 0');
+    $('.time').text('00' + ":" + '00');
+    this.isOn = false;
+  };
 }
 
-// function clearTimer(display) {
-//   clearInterval();
-// }
 
 jQuery(function($) {
 
   $('input[value="Start"]').on('click', function(event) {
-    // time in seconds
     var workVal = $('.work-val').val();
-    // var breakVal = $('.break-val').val();
-    var workDuration = workVal * 60;
-    // var breakDuration = breakVal * 60;
-    var display = $('.time');
+    var timer = workVal * 60;
+    var pomodoro = new TimerModule(timer);
 
-    console.log(workVal);
-    startTimer(workDuration, display);
+    pomodoro.start();
+
+    $('input[value="Reset"]').on('click', function(event) {
+      pomodoro.reset();
+    });
+
   });
 
 
