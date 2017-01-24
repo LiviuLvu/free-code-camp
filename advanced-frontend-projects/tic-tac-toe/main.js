17892144191a7e6328d499c6be3fa64fcaff561f
Liviu Iancu <liviuiancu@gmx.com>
2017-01-24 23:07:35 +0200

    added functionality: user can place moves on a square once

--- advanced-frontend-projects/tic-tac-toe/main.js
+++ advanced-frontend-projects/tic-tac-toe/main.js
@@ -1,15 +1,26 @@
-// display select player on new game
-// hide select form after option is chosen, show on restart
+
 // ai move is triggered after human moved
-// prevent same square being used more than once - check square.text()
-// current player is winner if game not tie
 // check if game is over
+// current player is winner if game not tie
 // reset board on game over
+// show select player on restart
 
 // default value for human player
 var human = 'x';
+var movesRecorder = [0,0,0,0,0,0,0,0,0];
 
-$('input').on('click', function() {
-  human = $('input:checked').val();
-  $('form').css('display', 'none');
+// choose player
+$('.choice input').on('click', function() {
+  human = $(this).val();
+  // hide form after player is chosen
+  // $('.settings').css('display', 'none');
+  // display grid
+  $('.game-grid').css('display', 'block');
+});
+
+// place value to grid on click if box is empty
+$('.game-grid p').on('click', function() {
+  if ($(this).is(':empty')) {
+    $(this).text(human);
+  }
 });
