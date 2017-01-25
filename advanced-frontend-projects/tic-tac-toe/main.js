c3233bf90268f650a70513cba1894179a6412580
Liviu Iancu <liviuiancu@gmx.com>
2017-01-25 23:08:29 +0200

    record each move in array

--- advanced-frontend-projects/tic-tac-toe/main.js
+++ advanced-frontend-projects/tic-tac-toe/main.js
@@ -1,26 +1,81 @@
-
-// ai move is triggered after human moved
+// switch players after each move
+// ai move is triggered after activePlayer moved
 // check if game is over
 // current player is winner if game not tie
 // reset board on game over
 // show select player on restart
 
-// default value for human player
-var human = 'x';
-var movesRecorder = [0,0,0,0,0,0,0,0,0];
+// default value for activePlayer player
+var humanPlayer, aiPlayer;
+var turn, move;
+var movesRecorder = ['n','n','n','n','n','n','n','n','n'];
 
 // choose player
 $('.choice input').on('click', function() {
-  human = $(this).val();
+  humanPlayer = $(this).val();
+
+  if (humanPlayer === 'x') {
+    aiPlayer = 'o';
+  }
+  else {
+    aiPlayer = 'x';
+  }
+
   // hide form after player is chosen
-  // $('.settings').css('display', 'none');
-  // display grid
-  $('.game-grid').css('display', 'block');
+  $('.settings').css('display', 'none');
+  // show grid
+  // $('.game-grid').css('display', 'block');
+  turn = 'human';
+  humanMove();
 });
 
-// place value to grid on click if box is empty
-$('.game-grid p').on('click', function() {
-  if ($(this).is(':empty')) {
-    $(this).text(human);
+function humanMove() {
+  if (turn === 'human') {
+    console.log('human');
+    // write and record human player move
+    $('.game-grid p').on('click', function() {
+      // write symbol if box is empty
+      if ($(this).is(':empty')) {
+        // write value
+        $(this).text(humanPlayer);
+
+        // get index of clicked box
+        move = $('.game-grid p').index($(this));
+        recordMove();
+        turn = 'aiPlayer';
+        computerMove();
+      }
+    });
+
   }
-});
+}
+
+function computerMove() {
+  if (turn === 'aiPlayer') {
+    console.log('ai');
+    $('.game-grid p').on('click', function() {
+      if ($(this).is(':empty')) {
+        $(this).text(aiPlayer);
+        move = $('.game-grid p').index($(this));
+        recordMove();
+        turn = 'human';
+        humanMove();
+      }
+    });
+
+  }
+}
+
+// record the choice in the moves aray
+function recordMove() {
+  var activePlayer;
+  if (turn === 'human') {
+    activePlayer = humanPlayer;
+  }
+  else {
+    activePlayer = aiPlayer;
+  }
+
+  movesRecorder[move] = activePlayer;
+  console.log(movesRecorder);
+}
