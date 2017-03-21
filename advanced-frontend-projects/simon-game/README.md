9e954477fb4579bc01ca06de38ef2abbe4f97eaf
Liviu Iancu <liviuiancu@gmx.com>
2017-03-21 16:34:09 +0200

    added sound and hover state

--- advanced-frontend-projects/simon-game/README.md
+++ advanced-frontend-projects/simon-game/README.md
@@ -4,6 +4,7 @@ Live app:
 http://liviulvu.github.io/free-code-camp/advanced-frontend-projects/simon-game/  
 
 Custom font downloaded from FontsForWeb.com  
+Sounds from https://www.freesound.org/people/pinkyfinger/packs/4409/
 
 To do:  
 Implement simon game functionality  
--- advanced-frontend-projects/simon-game/main.js
+++ advanced-frontend-projects/simon-game/main.js
@@ -1,8 +1,52 @@
-// I am presented with a random series of button presses.
-// Each time I input a series of button presses correctly, I see the same series of button presses but with an additional step.
-// I hear a sound that corresponds to each button both when the series of button presses plays, and when I personally press a button.
-// If I press the wrong button, I am notified that I have done so, and that series of button presses starts again to remind me of the pattern so I can try again.
-// I can see how many steps are in the current series of button presses.
-// If I want to restart, I can hit a button to do so, and the game will return to a single step.
-// I can play in strict mode where if I get a button press wrong, it notifies me that I have done so, and the game restarts at a new random series of button presses.
-// I can win the game by getting a series of 20 steps correct. I am notified of my victory, then the game starts over.
+// add step after each correct answer
+// replay reminder on wrong answer
+// display step
+// win game on step 20 completed
+// show win message
+
+// restart btn function
+
+// strict mode btn function
+// reset game on wrong move
+// show error !!
+
+// create hghtlight for each button
+// hear sound on button press
+playArr = [1, 2, 3, 4];
+
+// read random number from array
+var rand = playArr[Math.floor(Math.random() * playArr.length)];
+console.log(rand);
+// light up button
+$('.b'+ rand).addClass('light');
+
+function playSound() {
+    var beep = new Audio('sound/note'+ rand +'.mp3');
+    beep.play();
+}
+
+
+setTimeout(function () {
+        $('.b'+ rand).removeClass('light');
+    }, 500);
+
+$('.btn').on('click', function(e) {
+    e.preventDefault();
+    console.log('sound');
+    playSound();
+});
+
+$(document).ready(function() {
+});
+
+$('#start').on('click', function() {
+    console.log('start game');
+});
+
+$('#strict').on('click', function() {
+    console.log('strict mode gameplay');
+});
+
+$('#reset').on('click', function() {
+    console.log('reset game');
+});
